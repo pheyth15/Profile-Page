3ce193f9bc82538dfff40d99a6bbf8c432425c08
Kevin <53475718+pheyth15@users.noreply.github.com>
2022-01-23 23:25:08 +0800

    Add files via upload
    uploaded

---
+++ profilePage.js
@@ -0,0 +1,5 @@
+//Alert Button for "About Me"
+function myFunction(){
+    alert("Hello! My name is Kevin Daniel Almirante"+"\n"+"Age: 19"+"\n"+"I'm currently a first year student at CVSU-Imus"+"\n"+"I hope you like my simple profile page");
+}
+
